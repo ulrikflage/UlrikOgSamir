b8f95490a61a21404d5b8e45ee21be559f6a52bc
ulrikflage <74598993+ulrikflage@users.noreply.github.com>
2021-04-06 12:35:53 +0200

    1.2

--- TicTacToe/tictactoe.py
+++ TicTacToe/tictactoe.py
@@ -8,4 +8,13 @@ with SOCKET as s:
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            else:
+                print(data)
+            conn.sendall(data)
 
