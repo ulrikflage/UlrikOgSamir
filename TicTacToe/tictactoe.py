6ad4980d79cb4ebbeb07d0ac5a77cc7cf4eccb5a
ulrikflage <74598993+ulrikflage@users.noreply.github.com>
2021-04-06 12:28:26 +0200

    v1.1.1

--- TicTacToe/tictactoe.py
+++ TicTacToe/tictactoe.py
@@ -5,7 +5,7 @@ PORT = 3050
 
 SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 with SOCKET as s:
-    s.bind(HOST, PORT)
+    s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
 
