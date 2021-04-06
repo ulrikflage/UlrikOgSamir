d0296972475dbdb5e1face715ab9b4de9625b395
ulrikflage <74598993+ulrikflage@users.noreply.github.com>
2021-04-06 13:23:10 +0200

    1.2.1

--- TicTacToe/tictactoe.py
+++ TicTacToe/tictactoe.py
@@ -4,17 +4,22 @@ HOST = '192.168.100.100'
 PORT = 3050
 
 SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-with SOCKET as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            else:
-                print(data)
-            conn.sendall(data)
 
+
+def server(Socket, Host, Port):
+    with Socket as s:
+        s.bind((Host, Port))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                else:
+                    print(data)
+                conn.sendall(data)
+
+
+server(SOCKET, HOST, PORT)
