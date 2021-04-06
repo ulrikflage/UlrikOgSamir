91b55f4026a77dfc7b87a9570259f99a5a8d892d
ulrikflage <74598993+ulrikflage@users.noreply.github.com>
2021-04-06 14:01:37 +0200

    1.5

--- TicTacToe/Coppy server.py
+++ TicTacToe/Coppy server.py
@@ -19,18 +19,15 @@ def constructBoard():
 
 
 def printBoard():
-    print
-    '\nCurrent Board'
+    print('\nCurrent Board')
     for row in board:
-        print
-        row
-    print
-    '\n'
+        print(row)
+    print('\n')
 
 
 def piece(currentPlayer):
     piece = 'Null'
-    if (currentPlayer == player1):
+    if currentPlayer == player1:
         piece = ' X '
     else:
         piece = ' O '
@@ -48,8 +45,8 @@ def checkWinner(board, position, currentPlayer, sock):
                 if element in player1M:
                     marks += 1
                     if marks == 3:
-                        print
-                        player1, ' is the winnner!!!\nFinished'
+                        print(player1, ' is the winnner!!!\nFinished')
+
                         sock.send('%s is the winner!!!\nFinished' % player1)
                         return True
             marks = 0
@@ -61,21 +58,20 @@ def checkWinner(board, position, currentPlayer, sock):
                 if element in player2M:
                     marks += 1
                     if marks == 3:
-                        print
-                        player2, ' is the winnner!!!\nFinished'
+                        print(player2, ' is the winnner!!!\nFinished')
                         sock.send('%s is the winner!!!\nFinished' % player2)
                         return True
             marks = 0
     if len(player1M) + len(player2M) == 9:
-        print
-        'Tied Game!'
+        print('Tied Game!')
+
         sock.send('Tied Game!')
         return True
     return False
 
 
 def changePlayer(currentPlayer):
-    if (currentPlayer == player1):
+    if currentPlayer == player1:
         currentPlayer = player2
     else:
         currentPlayer = player1
@@ -92,10 +88,11 @@ def theGame(location, sock, currentPlayer):
         except:
             if currentPlayer == player2:
                 sock.send(
-                    '\nError - Reasons:\n1.Position is already taken.\n2. There is a tie\n3.An invalid entry.\n Try again')
+                    '\nError - Reasons:\n1.Position is already taken.\n2. There is a tie\n3.An invalid entry.\n Try '
+                    'again')
             else:
-                print
-                '\nError - Reasons:\n1.Position is already taked.\n2. There is a tie\n3.An invalid entry.\n Try again'
+                print('\nError - Reasons:\n1.Position is already taked.\n2. There is a tie\n3.An invalid entry.\n Try '
+                      'again')
 
     elif int(position) in range(4, 7):
         try:
@@ -105,10 +102,12 @@ def theGame(location, sock, currentPlayer):
         except:
             if currentPlayer == player2:
                 sock.send(
-                    '\nError - Reasons:\n1.Position is already taken.\n2. There is a tie\n3.An invalid entry.\n Try again')
+                    '\nError - Reasons:\n1.Position is already taken.\n2. There is a tie\n3.An invalid entry.\n Try '
+                    'again')
             else:
-                print
-                '\nError - Reasons:\n1.Position is already taked.\n2. There is a tie\n3.An invalid entry.\n Try again'
+                print('\nError - Reasons:\n1.Position is already taked.\n2. There is a tie\n3.An invalid entry.\n Try '
+                      'again')
+
     elif int(position) in range(7, 10):
         try:
             board[2][board[2].index(' %s ' % position)] = piece(currentPlayer)
@@ -117,60 +116,58 @@ def theGame(location, sock, currentPlayer):
         except:
             if currentPlayer == player2:
                 sock.send(
-                    '\nError - Reasons:\n1.Position is already taken.\n2. There is a tie\n3.An invalid entry.\n Try again')
+                    '\nError - Reasons:\n1.Position is already taken.\n2. There is a tie\n3.An invalid entry.\n Try '
+                    'again')
             else:
-                print
-                '\nError - Reasons:\n1.Position is already taked.\n2. There is a tie\n3.An invalid entry.\n Try again'
+                print('\nError - Reasons:\n1.Position is already taked.\n2. There is a tie\n3.An invalid entry.\n Try '
+                      'again')
+
     else:
         if currentPlayer == player2:
             sock.send('Invalid position. Try again')
         else:
-            print
-            'Invalid position. Try again'
+            print('Invalid position. Try again')
 
 
-print
-'\nWelcome to Tic-Tac-Toe over LAN\nYou are player 1 acting as the server\n\nEnter your private IP address: '
-host = raw_input()
+print('\nWelcome to Tic-Tac-Toe over LAN\nYou are player 1 acting as the server\n\nEnter your private IP address: ')
+
+host = input()
 
 s = socket.socket()
 port = 21217
 s.bind((host, port))
-print
-'\n\nThis game is running on %s:%s\n\nWaiting for connection...' % (host, port)
+print('\n\nThis game is running on %s:%s\n\nWaiting for connection...' % (host, port))
+
 s.listen(1)
 sock, addr = s.accept()
 while True:
-    print
-    'Got connection from', addr, '\n\n'
-    print
-    'Waiting for player 2...\n'
+    print('Got connection from', addr, '\n\n')
+
+    print('Waiting for player 2...\n')
+
     a = "Connected to %s\n\nEnter your player name: " % host
-    sock.send(str(a))
+    sock.send(bytes(str(a)))
     player2 = sock.recv(4096)
-    print
-    'Enter your player name: '
-    player1 = raw_input()
-    players.append(player1)
-    players.append(player2)
+    print('Enter your player name: ')
+
+    player1 = input()
+    players.append(str(player1))
+    players.append(str(player2))
     play = '\n\nPlayers\n1. %s\n2. %s\n\n%s' % (player1, player2, constructBoard())
-    print
-    play
-    sock.send(play)
+    print(play)
+    sock.send(bytes(play))
     currentPlayer = random.choice(players)
-    while isFinished == False:
+    while not isFinished:
         if currentPlayer == player1:
-            print
-            '\n\nIt is your turn. %s place your piece' % player1
-            sock.send("\n\nIt is %s's turn. Please wait..." % player1)
-            position = raw_input()
+            print('\n\nIt is your turn. %s place your piece' % player1)
+            sock.send(b"\n\nIt is %s's turn. Please wait..." % player1)
+            position = input()
             theGame(position, sock, currentPlayer)
             isFinished = checkWinner(board, position, currentPlayer, sock)
             currentPlayer = changePlayer(currentPlayer)
         else:
-            print
-            "\n\nIt is %s's turn. Please wait..." % player2
-            sock.send("\n\nIt is your turn. %s place your piece" % player2)
+            print("\n\nIt is %s's turn. Please wait..." % player2)
+            sock.send(b"\n\nIt is your turn. %s place your piece" % player2)
             position = sock.recv(4096)
             theGame(position, sock, currentPlayer)
             isFinished = checkWinner(board, position, currentPlayer, sock)
--- TicTacToe/coppy client.py
+++ TicTacToe/coppy client.py
@@ -1,30 +1,31 @@
-#Tic-Tac-Toe over LAN
-#player2.py acts as the client
+# Tic-Tac-Toe over LAN
+# player2.py acts as the client
 import socket
 
-print '\nWelcome to Tic-Tac-Toe over LAN\nYou are player 2 acting as the client\n\nEnter the private IP address of player 1: '
-host=raw_input()
+print('\nWelcome to Tic-Tac-Toe over LAN\nYou are player 2 acting as the client\n\nEnter the private IP address of '
+      'player 1: ')
+host = input()
 
 s = socket.socket()
 port = 21217
 
 s.connect((host, port))
 
-print s.recv(4096)
-playername=raw_input()
-s.send(playername)
-print '\nWaiting for player 1...'
-print s.recv(4096)
+print(s.recv(4096))
+playername = input()
+s.send(bytes(playername))
+print('\nWaiting for player 1...')
+print(s.recv(4096))
 while True:
-	data=s.recv(4096)
-	if 'piece' in data:
-		print data
-		position=raw_input()
-		s.send(position)
-	else:
-		print data
+    data = s.recv(4096)
+    if b'piece' in data:
+        print(data)
+        position = input()
+        s.send(bytes(position))
+    else:
+        print(data.decode())
 
-	if 'Finished' in data:
-		break
+    if b'Finished' in data:
+        break
 
 s.close()
