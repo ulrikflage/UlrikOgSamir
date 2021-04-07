c634b28d5aa510b4c61de8035ae01b944feead96
Yami-Sam <65710743+Yami-Sam@users.noreply.github.com>
2021-04-07 08:18:30 +0200

    Commit

--- TicTacToe/client.py
+++ TicTacToe/client.py
@@ -1,6 +1,6 @@
 import socket
 
-HOST = '192.168.100.100'  # The server's hostname or IP address
+HOST = '172.18.170.214'  # The server's hostname or IP address
 PORT = 3050        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
