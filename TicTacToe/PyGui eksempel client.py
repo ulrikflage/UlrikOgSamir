057e195925d4d12f1c6507c2e8517c107a92d869
ulrikflage <74598993+ulrikflage@users.noreply.github.com>
2021-04-08 14:49:17 +0200

    PyGui 1.1

--- TicTacToe/PyGui eksempel client.py
+++ TicTacToe/PyGui eksempel client.py
@@ -9,9 +9,8 @@ s.bind((HOST, PORT))
 
 koblet_til = False
 
-#player = 'O'
-enemy_symbol = 'O'
-symbol = 'X'
+player = 'O'
+symbol = ''
 
 font = 'Helvetica 60'
 sg.theme('Default 1')
@@ -33,55 +32,52 @@ class Client(Thread):
         Thread.__init__(self)
         self.sock = socket
         self.addr = address
-        self.client_data = b''
-        self.data = ''
         self.start()
 
     def run(self):
-        self.gui()
         while 1:
-            self.data = self.sock.recv(1024).decode()
-            self.sock.send(self.client_data)
-            self.client_data = None
+            print('Client sent: ', self.sock.recv(1024).decode())
+            self.sock.send(b'')
 
     def gui(self):
         global symbol
-        global enemy_symbol
         while True:
             event, values = window.Read(timeout=20)
-
-            if self.data:
-                window.FindElement(self.data).update(enemy_symbol)
+            if True:
+                symbol = 'X'
             if event == sg.WIN_CLOSED:
                 break
             elif event == '1':
                 window.FindElement(event).Update(symbol)
-                self.client_data = b'1'
+                s.send(bytes(event))
             elif event == '2':
                 window.FindElement(event).Update(symbol)
-                self.client_data = b'2'
+                conn.send(event).encode()
             elif event == '3':
                 window.FindElement(event).Update(symbol)
-                self.client_data = b'3'
+                conn.send(event).encode()
             elif event == '4':
                 window.FindElement(event).Update(symbol)
-                self.client_data = b'4'
+                conn.send(event).encode()
             elif event == '5':
                 window.FindElement(event).Update(symbol)
-                self.client_data = b'5'
+                conn.send(event).encode()
             elif event == '6':
                 window.FindElement(event).Update(symbol)
-                self.client_data = b'6'
+                conn.send(event).encode()
             elif event == '7':
                 window.FindElement(event).Update(symbol)
-                self.client_data = b'7'
+                conn.send(event).encode()
             elif event == '8':
                 window.FindElement(event).Update(symbol)
-                self.client_data = b'8'
+                conn.send(event).encode()
             elif event == '9':
                 window.FindElement(event).Update(symbol)
-                self.client_data = b'9'
+                conn.send(event).encode()
         window.close()
+
+
+
 
 
 s.listen(5)
