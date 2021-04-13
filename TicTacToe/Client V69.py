51042890d4e55ebee07293be11549e3ebf41635e
Yami-Sam <65710743+Yami-Sam@users.noreply.github.com>
2021-04-13 13:00:19 +0200

    l

--- TicTacToe/Client V69.py
+++ TicTacToe/Client V69.py
@@ -1,29 +1,26 @@
-import os
 import pygame
-import socket
-import threading
 from grid import Grid
 
+import os
 os.environ['SDL_VIDEO_WINDOW_POS'] = '850,100'
 
 surface = pygame.display.set_mode((600,600))
-pygame.display.set_caption('Tic-tac-toe | Client')
-
+pygame.display.set_caption('Tic-tac-toe')
 
 # create a separate thread to send and receive data from the server
+import threading
 def create_thread(target):
     thread = threading.Thread(target=target)
     thread.daemon = True
     thread.start()
 
-
 # creating a TCP socket for the client
-HOST = '192.168.0.100'
+import socket
+HOST = '127.0.0.1'
 PORT = 65432
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
-
 
 def receive_data():
     global turn
@@ -37,7 +34,6 @@ def receive_data():
             grid.game_over = True
         if grid.get_cell_value(x, y) == 0:
             grid.set_cell_value(x, y, 'X')
-
 
 # run the blocking functions in a separate thread
 create_thread(receive_data)
@@ -72,7 +68,7 @@ while running:
             elif event.key == pygame.K_ESCAPE:
                 running = False
 
-    surface.fill((255, 255, 255))
+    surface.fill((0,0,0))
 
     grid.draw(surface)
 
--- TicTacToe/Server V69.py
+++ TicTacToe/Server V69.py
@@ -1,34 +1,34 @@
-import os
 import pygame
-import socket
-import threading
 from grid import Grid
 
-os.environ['SDL_VIDEO_WINDOW_POS'] = '850,100'
+import os
+os.environ['SDL_VIDEO_WINDOW_POS'] = '200,100'
 
 surface = pygame.display.set_mode((600,600))
-pygame.display.set_caption('Tic-tac-toe | Client')
+pygame.display.set_caption('Tic-tac-toe')
 
-
-# create a separate thread to send and receive data from the server
+# create a separate thread to send and receive data from the client
+import threading
 def create_thread(target):
     thread = threading.Thread(target=target)
     thread.daemon = True
     thread.start()
 
-
-# creating a TCP socket for the client
-HOST = '192.168.0.100'
+# creating a TCP socket for the server
+import socket
+HOST = '127.0.0.1'
 PORT = 65432
+connection_established = False
+conn, addr = None, None
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST, PORT))
-
+sock.bind((HOST, PORT))
+sock.listen(1)
 
 def receive_data():
     global turn
     while True:
-        data = sock.recv(1024).decode() # receive data from the server, it is a blocking method
+        data = conn.recv(1024).decode() # receive data from the client, it is a blocking method
         data = data.split('-') # the format of the data after splitting is: ['x', 'y', 'yourturn', 'playing']
         x, y = int(data[0]), int(data[1])
         if data[2] == 'yourturn':
@@ -36,23 +36,29 @@ def receive_data():
         if data[3] == 'False':
             grid.game_over = True
         if grid.get_cell_value(x, y) == 0:
-            grid.set_cell_value(x, y, 'X')
+            grid.set_cell_value(x, y, 'O')
 
+def waiting_for_connection():
+    global connection_established, conn, addr
+    conn, addr = sock.accept() # wait for a connection, it is a blocking method
+    print('client is connected')
+    connection_established = True
+    receive_data()
 
 # run the blocking functions in a separate thread
-create_thread(receive_data)
+create_thread(waiting_for_connection)
 
 grid = Grid()
 running = True
-player = "O"
-turn = False
+player = "X"
+turn = True
 playing = 'True'
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN and not grid.game_over:
+        if event.type == pygame.MOUSEBUTTONDOWN and connection_established:
             if pygame.mouse.get_pressed()[0]:
                 if turn and not grid.game_over:
                     pos = pygame.mouse.get_pos()
@@ -61,7 +67,7 @@ while running:
                     if grid.game_over:
                         playing = 'False'
                     send_data = '{}-{}-{}-{}'.format(cellX, cellY, 'yourturn', playing).encode()
-                    sock.send(send_data)
+                    conn.send(send_data)
                     turn = False
 
         if event.type == pygame.KEYDOWN:
@@ -72,7 +78,8 @@ while running:
             elif event.key == pygame.K_ESCAPE:
                 running = False
 
-    surface.fill((255, 255, 255))
+
+    surface.fill((0,0,0))
 
     grid.draw(surface)
 
