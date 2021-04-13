7094d4c4f186f440e702a71f2478ebc0c1eea0af
ulrikflage <74598993+ulrikflage@users.noreply.github.com>
2021-04-13 13:22:52 +0200

    points

--- TicTacToe/rid.py
+++ TicTacToe/rid.py
@@ -16,6 +16,8 @@ class Grid:
         # search directions  N         NW        W       SW       S       SE      E       NE
         self.search_dirs = [(0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1)]
         self.game_over = False
+        self.o_points = 0
+        self.x_points = 0
 
     def draw(self, surface):
         for line in self.grid_lines:
@@ -84,6 +86,11 @@ class Grid:
         if count == 3:
             print(player, 'wins!')
             self.game_over = True
+            if player == 'O':
+                self.o_points += 1
+            elif player == 'X':
+                self.x_points += 1
+            print("\nO:", self.o_points, "\nX:", self.x_points, "\n")
         else:
             self.game_over = self.is_grid_full()
 
